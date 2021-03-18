ddfc88e1bf30b7b2d27143bfb506728a5f617395
Issa-barry <58512660+Issa-barry@users.noreply.github.com>
2021-03-18 11:15:34 +0100

    Delete js/components directory

--- js/components/mySliderPreview/index.js
+++
@@ -1,9 +0,0 @@
-class mySliderPreview extends HTMLElement {
-
-	connectedCallback(){
-		this.appendChild(document.createElement('img'))
-	}
-
-}
-
-customElements.define('my-slider-preview', mySliderPreview);
